f2d650873c35b6ba85bb8cf43aa74d101c8dfb80
lynxgamestudio <tccmemoriasemchamas7@gmail.com>
2024-05-16 19:49:45 -0300

    a

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,17 @@
-function modal(){
-    
+var token = false;
+function openModal(){
+    if(token == false){
+        token = true;
+        document.getElementById("myModal").style.display = "block";
+    }
+    else{
+        token = false;
+        document.getElementById("myModal").style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById("myModal")) {
+      document.getElementById("myModal").style.display = "none";
+    }
 }
